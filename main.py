08176281472830911865d3bd5c4ece26df171da7
kranik <optimalx@mail.ru>
2026-06-26 13:37:24 +0500

    feat(main): add scheduler

--- main.py
+++ main.py
@@ -2,17 +2,39 @@ import asyncio
 import logging
 import sys
 
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from apscheduler.triggers.interval import IntervalTrigger
+
+from app.tasks import publish
+
+INTERVAL = 5  # minutes
+
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s | %(name)s | %(levelname)s | %(message)s",
     force=True,
+    stream=sys.stdout,
 )
 
 logger = logging.getLogger(__name__)
 
+shutdown_event = asyncio.Event()
+
 
 async def main():
+    scheduler = AsyncIOScheduler(logger=logger)
+    scheduler.add_job(
+        publish,
+        trigger=IntervalTrigger(minutes=INTERVAL),
+        id="publish_job",
+        coalesce=True,
+        max_instances=1,
+    )
+    scheduler.start()
+
     logger.info("Запуск приложения...")
+
+    await shutdown_event.wait()
 
 
 if __name__ == "__main__":
